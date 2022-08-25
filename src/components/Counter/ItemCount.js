7991327906de33bf03d9bf97bdc6138cb73165a7
Diego Bastida <diegoabastida@gmail.com>
2022-08-24 22:22:58 -0300

    Counter

---
+++ src/components/Counter/ItemCount.js
@@ -0,0 +1,68 @@
+import React,{Component, createElement} from 'react';
+
+
+let stock = 5;
+
+
+class Contador extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			counter: 0,
+			stockCarrito: 0
+		};
+
+	}
+
+	handlerCounterUp = () => {
+		if (this.state.counter < stock && stock > 0) {
+			this.setState({ counter: this.state.counter + 1 });
+		} else {
+			alert("Sin Stock")
+		};
+		
+	};
+	handlerCounterDown = () => {
+		if (this.state.counter > 0) {
+			this.setState({ counter: this.state.counter - 1 });
+		}
+	};
+
+	handlerCounterAdd = () =>{
+		if(this.state.counter > 0 && this.state.stockCarrito >= 0 && stock > 0 && this.state.counter <= stock) {
+			this.setState({stockCarrito: this.state.counter + this.state.stockCarrito});
+			stock = stock - this.state.counter;
+		}else{
+			alert("Sin Stock")
+		}
+	}
+
+	handlerCounterRemove=()=>{
+		if(this.state.stockCarrito > 0 && stock >= 0){
+			stock = this.state.stockCarrito + stock;
+			this.setState({stockCarrito: 0})
+		}
+	}
+
+	render() {
+		return ( 
+				
+				<div className='CounterSection'>
+					<p>Stock Total {stock}</p>
+					<p>Cantidad Seleccionada: {this.state.counter}</p>
+					<div className='btn-section'>
+						<button onClick={this.handlerCounterUp}>+</button>
+						<button onClick={this.handlerCounterDown}>-</button>
+					</div>
+					<div>
+						<p>Stock en Carrito {this.state.stockCarrito}</p>
+						<button onClick={this.handlerCounterAdd}>Agregar</button>
+						<button onClick={this.handlerCounterRemove}>Vaciar</button>
+
+					</div>
+				</div>
+			
+		);
+	}
+} export default Contador;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Title from "../Title/Title";
+import Contador from "../Counter/ItemCount";
 
 const ItemListContainer = () => {
     return(
+        <div>
         <Title greeting="Diego Bastida" />
+        <Contador />
+        </div>
     );
 }
 
