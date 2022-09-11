f8a2435d03e2eeacaea72f968dad216fc8bfb813
Diego Bastida <diegoabastida@gmail.com>
2022-09-11 18:13:58 -0300

    Entrega

--- src/view/ItemDetailContainer/ItemDetail.js
+++ src/view/ItemDetailContainer/ItemDetail.js
@@ -2,13 +2,15 @@ import * as React from 'react';
 import ItemCount from '../../components/Counter/ItemCount';
 import { useCartContext } from '../../Context/CartContext';
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
 
 const ItemDetail = ({ data }) => {
+	const [goToCart, setGoToCart] = useState(false);
 	const {addProduct} = useCartContext();
 
 	const onAdd = (quantity) =>{
-		console.log(`compraste ${quantity}`)
+		setGoToCart(true)
+		alert(`Añadiste ${quantity} unidades al carrito`)
 		addProduct(data, quantity);
 	}
 
@@ -18,10 +20,9 @@ const ItemDetail = ({ data }) => {
 			<img src={data.images} alt="Object" />
 			<p>Price: $ {data.price}</p>
 			<p>Description: {data.description} </p>
-			<ItemCount initial={1} stock={15} onAdd={onAdd} /> 
-			<Link to="/cart">
-				<h2>Ir al carrito</h2>
-			</Link> 
+			{
+				goToCart ? <Link to="/cart"><h2>Ir al carrito</h2></Link> : <ItemCount initial={1} stock={15} onAdd={onAdd} />
+			}
 		</div>
 			
 	);
